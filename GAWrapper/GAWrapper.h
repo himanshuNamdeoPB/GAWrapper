84732cdf17d290dccc67fa6b715650bbcaf72ca7
Himanshu <himanshu.namdeo@pb.com>
2018-12-04 15:41:24 +0530

    Initial commit

---
+++ GAWrapper/GAWrapper.h
@@ -0,0 +1,19 @@
+//
+//  GAWrapper.h
+//  GAWrapper
+//
+//  Created by Himanshu on 04/12/18.
+//  Copyright © 2018 Pitney Bowes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GAWrapper.
+FOUNDATION_EXPORT double GAWrapperVersionNumber;
+
+//! Project version string for GAWrapper.
+FOUNDATION_EXPORT const unsigned char GAWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GAWrapper/PublicHeader.h>
+
+
